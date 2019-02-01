de2a86a91edd3521e2dc2acd77ef46713085a5b6
Trent Williams <proteinshakedev@gmail.com>
2019-02-01 16:54:36 -0500

    Save GenBlocks on server close and changed MineageCore#getInstance usage to a const field.

--- src/main/java/rip/simpleness/mineagecore/modules/ModuleGenBlock.java
+++ src/main/java/rip/simpleness/mineagecore/modules/ModuleGenBlock.java
@@ -1,9 +1,11 @@
 package rip.simpleness.mineagecore.modules;
 
 import com.google.common.collect.ImmutableMap;
+import com.google.common.reflect.TypeToken;
 import me.lucko.helper.Events;
 import me.lucko.helper.event.filter.EventFilters;
 import me.lucko.helper.serialize.BlockPosition;
+import me.lucko.helper.serialize.GsonStorageHandler;
 import me.lucko.helper.terminable.TerminableConsumer;
 import me.lucko.helper.terminable.module.TerminableModule;
 import me.lucko.helper.text.Text;
@@ -19,9 +21,11 @@ import rip.simpleness.mineagecore.objs.Generation;
 
 import javax.annotation.Nonnull;
 import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class ModuleGenBlock implements TerminableModule {
 
+    private static final MineageCore INSTANCE = MineageCore.getInstance();
     private static final ImmutableMap<String, GenBlock> genBlockMap = ImmutableMap.<String, GenBlock>builder()
             .put("vertical-cobblestone", new GenBlock(Material.COBBLESTONE, Direction.DOWN, 50))
             .put("horizontal-cobblestone", new GenBlock(Material.COBBLESTONE, Direction.HORIZONTAL, 100))
@@ -33,34 +37,41 @@ public class ModuleGenBlock implements TerminableModule {
             .put("vertical-sand-cannon", new GenBlock(Material.SAND, Direction.UP, 5000, true))
             .build();
 
+    private GsonStorageHandler<ConcurrentHashMap<UUID, Generation>> gsonStorageHandler;
+
     @Override
     public void setup(@Nonnull TerminableConsumer terminableConsumer) {
+        this.gsonStorageHandler = new GsonStorageHandler<>("genblocks", ".json", INSTANCE.getDataFolder(), new TypeToken<ConcurrentHashMap<UUID, Generation>>() {
+        });
+        INSTANCE.getGenerationTask().setGenerations(gsonStorageHandler.load().orElse(new ConcurrentHashMap<>()));
+        terminableConsumer.bind(() -> gsonStorageHandler.save(INSTANCE.getGenerationTask().getGenerations()));
+
         genBlockMap.forEach((key, value) -> new CustomItem(key, value.buildItemStack()));
         Events.subscribe(BlockPlaceEvent.class)
                 .filter(EventFilters.ignoreCancelled())
                 .filter(event -> event.getItemInHand() != null)
                 .filter(event -> event.getBlockPlaced() != null)
-                .filter(event -> MineageCore.getInstance().canBuild(event.getPlayer(), event.getBlockPlaced().getLocation()))
+                .filter(event -> INSTANCE.canBuild(event.getPlayer(), event.getBlockPlaced().getLocation()))
                 .handler(event -> {
                     CustomItem customItem = CustomItem.getCustomItem(event.getItemInHand());
                     if (customItem != null) {
                         GenBlock genBlock = genBlockMap.get(customItem.getId());
                         if (genBlock != null) {
                             Player player = event.getPlayer();
-                            if (MineageCore.getInstance().getEconomy().getBalance(player) < genBlock.getPrice()) {
+                            if (INSTANCE.getEconomy().getBalance(player) < genBlock.getPrice()) {
                                 player.sendMessage(Text.colorize("&cYou don't have enough money to place this genblock!"));
                                 event.setCancelled(true);
                             } else {
-                                MineageCore.getInstance().getEconomy().withdrawPlayer(player, genBlock.getPrice());
+                                INSTANCE.getEconomy().withdrawPlayer(player, genBlock.getPrice());
                                 BlockFace blockFace = genBlock.getDirection().toBlockFace();
                                 if (blockFace == null) {
                                     blockFace = genBlock.getDirection().toBlockFace(event.getBlockAgainst(), event.getBlockPlaced());
-                                    MineageCore.getInstance()
+                                    INSTANCE
                                             .getGenerationTask()
                                             .getGenerations()
                                             .put(UUID.randomUUID(), new Generation(genBlock, BlockPosition.of(event.getBlockPlaced()), blockFace));
                                 }
-                                MineageCore.getInstance()
+                                INSTANCE
                                         .getGenerationTask()
                                         .getGenerations()
                                         .put(UUID.randomUUID(), new Generation(genBlock, BlockPosition.of(event.getBlockPlaced()), blockFace));
