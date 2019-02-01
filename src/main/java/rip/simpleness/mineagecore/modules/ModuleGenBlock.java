f7864bafb1e2644c4668ff332476b316a83db471
Trent Williams <proteinshakedev@gmail.com>
2019-02-01 15:01:07 -0500

    Removed genblockbenchmark and added more genbucket types

--- src/main/java/rip/simpleness/mineagecore/modules/ModuleGenBlock.java
+++ src/main/java/rip/simpleness/mineagecore/modules/ModuleGenBlock.java
@@ -1,7 +1,6 @@
 package rip.simpleness.mineagecore.modules;
 
 import com.google.common.collect.ImmutableMap;
-import me.lucko.helper.Commands;
 import me.lucko.helper.Events;
 import me.lucko.helper.event.filter.EventFilters;
 import me.lucko.helper.serialize.BlockPosition;
@@ -9,9 +8,7 @@ import me.lucko.helper.terminable.TerminableConsumer;
 import me.lucko.helper.terminable.module.TerminableModule;
 import me.lucko.helper.text.Text;
 import org.bukkit.Material;
-import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
-import org.bukkit.craftbukkit.v1_8_R3.block.CraftBlock;
 import org.bukkit.entity.Player;
 import org.bukkit.event.block.BlockPlaceEvent;
 import rip.simpleness.mineagecore.MineageCore;
@@ -25,30 +22,20 @@ import java.util.UUID;
 
 public class ModuleGenBlock implements TerminableModule {
 
-    private static final ImmutableMap<String, GenBlock> genBlockMap = ImmutableMap.of(
-            "vertical-cobblestone", new GenBlock(Material.COBBLESTONE, Direction.DOWN, 50),
-            "horizontal-cobblestone", new GenBlock(Material.COBBLESTONE, Direction.HORIZONTAL, 100),
-            "vertical-obsidian", new GenBlock(Material.OBSIDIAN, Direction.DOWN, 2500),
-            "horizontal-obsidian", new GenBlock(Material.OBSIDIAN, Direction.HORIZONTAL, 3000),
-            "vertical-sand", new GenBlock(Material.SAND, Direction.UP, 2500));
+    private static final ImmutableMap<String, GenBlock> genBlockMap = ImmutableMap.<String, GenBlock>builder()
+            .put("vertical-cobblestone", new GenBlock(Material.COBBLESTONE, Direction.DOWN, 50))
+            .put("horizontal-cobblestone", new GenBlock(Material.COBBLESTONE, Direction.HORIZONTAL, 100))
+            .put("vertical-obsidian", new GenBlock(Material.OBSIDIAN, Direction.DOWN, 2500))
+            .put("horizontal-obsidian", new GenBlock(Material.OBSIDIAN, Direction.HORIZONTAL, 3000))
+            .put("vertical-sand", new GenBlock(Material.SAND, Direction.UP, 2500))
+            .put("vertical-cobblestone-patch", new GenBlock(Material.COBBLESTONE, Direction.DOWN, 250, true))
+            .put("vertical-obsidian-patch", new GenBlock(Material.COBBLESTONE, Direction.DOWN, 250, true))
+            .put("vertical-sand-cannon", new GenBlock(Material.SAND, Direction.UP, 5000, true))
+            .build();
 
     @Override
     public void setup(@Nonnull TerminableConsumer terminableConsumer) {
         genBlockMap.forEach((key, value) -> new CustomItem(key, value.buildItemStack()));
-        Commands.create()
-                .assertPermission("mineage.admin")
-                .handler(commandContext -> {
-                    Player player = (Player) commandContext.sender();
-                    GenBlock genBlock = genBlockMap.get("vertical-cobblestone");
-                    for (int x = -50; x < 50; x++) {
-                        for (int z = -50; z < 50; z++) {
-                            Block block = player.getLocation().add(x, 0, z).getBlock();
-                            ((CraftBlock) block).setTypeIdAndData(genBlock.getMaterial().getId(), (byte) 0, false);
-                            MineageCore.getInstance().getGenerationTask().getGenerations().put(UUID.randomUUID(), new Generation(genBlock, BlockPosition.of(block), BlockFace.DOWN));
-                        }
-                    }
-                }).registerAndBind(terminableConsumer, "genblockbenchmark");
-
         Events.subscribe(BlockPlaceEvent.class)
                 .filter(EventFilters.ignoreCancelled())
                 .filter(event -> event.getItemInHand() != null)
