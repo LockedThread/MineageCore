62dc1391cc26b41cea4ce3fa93579de680e3a705
Trent Williams <proteinshakedev@gmail.com>
2019-02-03 22:00:41 -0500

    Implemented panes and proper items

--- src/main/java/rip/simpleness/mineagecore/menus/MenuGenBlock.java
+++ src/main/java/rip/simpleness/mineagecore/menus/MenuGenBlock.java
@@ -1,7 +1,17 @@
 package rip.simpleness.mineagecore.menus;
 
+import me.lucko.helper.item.ItemStackBuilder;
 import me.lucko.helper.text.Text;
+import org.bukkit.DyeColor;
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.ItemFlag;
+import org.bukkit.inventory.ItemStack;
+import rip.simpleness.mineagecore.customitems.CustomItem;
 import rip.simpleness.mineagecore.modules.ModuleGenBlock;
+import rip.simpleness.mineagecore.objs.GenBlock;
+
+import java.util.Map;
 
 public class MenuGenBlock extends Menu {
 
@@ -12,16 +22,24 @@ public class MenuGenBlock extends Menu {
 
     @Override
     public void setup() {
-        ModuleGenBlock.genBlockMap
-                .values()
-                .forEach(value -> setItem(getFirstEmpty(), new MenuIcon(value.buildGuiItemStack()).setEvent(event -> {
-                    if (event.getWhoClicked().getInventory().firstEmpty() == -1) {
-                        event.getWhoClicked().sendMessage(Text.colorize("&cYour inventory is full!"));
-                        event.getWhoClicked().closeInventory();
-                    } else {
-                        event.getWhoClicked().getInventory().addItem(value.buildItemStack());
-                    }
-                    event.setCancelled(true);
-                })));
+        final ItemStack pane = ItemStackBuilder.of(Material.STAINED_GLASS_PANE)
+                .data(DyeColor.BLACK.getData())
+                .flag(ItemFlag.HIDE_ENCHANTS)
+                .enchant(Enchantment.DURABILITY)
+                .name(" ")
+                .build();
+        setItem(0, pane);
+        setItem(8, pane);
+        for (Map.Entry<String, GenBlock> entry : ModuleGenBlock.genBlockMap.entrySet()) {
+            setItem(getFirstEmpty(), new MenuIcon(entry.getValue().buildGuiItemStack()).setEvent(event -> {
+                if (event.getWhoClicked().getInventory().firstEmpty() == -1) {
+                    event.getWhoClicked().sendMessage(Text.colorize("&cYour inventory is full!"));
+                    event.getWhoClicked().closeInventory();
+                } else {
+                    event.getWhoClicked().getInventory().addItem(CustomItem.getCustomItemHashMap().get(entry.getKey()).getItemStack());
+                }
+                event.setCancelled(true);
+            }));
+        }
     }
 }
