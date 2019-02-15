b9a3a7b798cc0a7d0b03606d88cc7d9296fa97df
Trent Williams <proteinshakedev@gmail.com>
2019-02-14 23:01:33 -0500

    Reformatting because long if statement

--- src/main/java/rip/simpleness/mineagecore/menus/MenuListener.java
+++ src/main/java/rip/simpleness/mineagecore/menus/MenuListener.java
@@ -9,7 +9,10 @@ public class MenuListener implements Listener {
 
     @EventHandler(ignoreCancelled = true)
     public void onInventoryClick(InventoryClickEvent event) {
-        if (event.getWhoClicked() instanceof Player && event.getClickedInventory() != null && event.getCurrentItem() != null && event.getClickedInventory().getHolder() instanceof Menu) {
+        if (event.getWhoClicked() instanceof Player &&
+                event.getClickedInventory() != null &&
+                event.getCurrentItem() != null &&
+                event.getClickedInventory().getHolder() instanceof Menu) {
             final MenuIcon icon = ((Menu) event.getInventory().getHolder()).getIcon(event.getRawSlot());
             if (icon != null && icon.getEvent() != null) {
                 icon.getEvent().accept(event);
