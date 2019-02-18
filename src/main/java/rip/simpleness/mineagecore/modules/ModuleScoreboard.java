95c2c828ba3f40018494095d31aa1511abf544d9
Trent Williams <proteinshakedev@gmail.com>
2019-02-18 18:38:58 -0500

    Disabled scoreboard

--- src/main/java/rip/simpleness/mineagecore/modules/ModuleScoreboard.java
+++ src/main/java/rip/simpleness/mineagecore/modules/ModuleScoreboard.java
@@ -16,6 +16,7 @@ import me.lucko.helper.utils.Players;
 import me.lucko.helper.utils.Tps;
 import org.apache.commons.lang3.StringUtils;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.player.PlayerChangedWorldEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
@@ -23,14 +24,19 @@ import org.bukkit.scoreboard.DisplaySlot;
 import rip.simpleness.mineagecore.MineageCore;
 
 import javax.annotation.Nonnull;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.text.DecimalFormat;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.UUID;
 import java.util.function.BiConsumer;
 import java.util.stream.Collectors;
 
 public class ModuleScoreboard implements TerminableModule {
 
+    public static HashSet<UUID> disabledScordboard;
     private static final MetadataKey<ScoreboardObjective> SCOREBOARD_OBJECTIVE_METADATA_KEY = MetadataKey.create("scoreboard", ScoreboardObjective.class);
     private static final MineageCore INSTANCE = MineageCore.getInstance();
     private PacketScoreboard scoreboard;
@@ -39,13 +45,18 @@ public class ModuleScoreboard implements TerminableModule {
 
     @Override
     public void setup(@Nonnull TerminableConsumer terminableConsumer) {
+        disabledScordboard = new HashSet<>();
         this.scoreboard = INSTANCE.getPacketScoreboardProvider().getScoreboard();
         this.scoreboardLines = INSTANCE.getConfig().getStringList("scoreboard.lines").stream().map(Text::colorize).collect(Collectors.toList());
-        this.scoreboardDisplayName = Text.colorize(INSTANCE.getConfig().getString("scoreboard.display-name"));
+        this.scoreboardDisplayName = INSTANCE.getConfig().getString("scoreboard.display-name");
 
         BiConsumer<Player, ScoreboardObjective> updater = (player, scoreboardObjective) -> {
+            if (disabledScordboard.contains(player.getUniqueId())) {
+                scoreboardObjective.unsubscribe(player);
+                return;
+            }
             FPlayer fPlayer = FPlayers.getInstance().getByPlayer(player);
-            scoreboardObjective.setDisplayName(scoreboardDisplayName);
+            scoreboardObjective.setDisplayName(Text.colorize(scoreboardDisplayName));
             HashMap<String, Integer> map = new HashMap<>();
             final List<String> reversed = Lists.reverse(scoreboardLines);
             for (int i = 0; i < reversed.size(); i++) {
@@ -53,7 +64,7 @@ public class ModuleScoreboard implements TerminableModule {
                 if (line.isEmpty()) {
                     map.put(StringUtils.repeat(" ", i), i);
                 } else {
-                    map.put(line.replace("{faction_name}", fPlayer.hasFaction() ? fPlayer.getFaction().getTag() : "&2Wilderness").replace("{balance}", String.valueOf(INSTANCE.getEconomy().getBalance(player))).replace("{tps}", new DecimalFormat("#.##").format(Tps.read().avg1())), i);
+                    map.put(line.replace("{faction_name}", fPlayer.hasFaction() ? fPlayer.getFaction().getTag() : "&2Wilderness").replace("{balance}", new BigDecimal(INSTANCE.getEconomy().getBalance(player)).setScale(2, RoundingMode.DOWN).toString()).replace("{tps}", new DecimalFormat("#.##").format(Tps.read().avg1())), i);
                 }
             }
             scoreboardObjective.applyScores(map);
@@ -62,13 +73,13 @@ public class ModuleScoreboard implements TerminableModule {
         Events.subscribe(PlayerChangedWorldEvent.class)
                 .handler(event -> updater.accept(event.getPlayer(), Metadata.provideForPlayer(event.getPlayer()).getOrNull(SCOREBOARD_OBJECTIVE_METADATA_KEY))).bindWith(terminableConsumer);
 
-        Events.subscribe(PlayerJoinEvent.class)
-                .handler(event -> {
-                    Player player = event.getPlayer();
-                    ScoreboardObjective scoreboardObjective = scoreboard.createPlayerObjective(player, "null", DisplaySlot.SIDEBAR);
+        Events.subscribe(PlayerJoinEvent.class, EventPriority.HIGHEST)
+                .handler(event -> Schedulers.sync().runLater(() -> {
+                    final Player player = event.getPlayer();
+                    final ScoreboardObjective scoreboardObjective = scoreboard.createPlayerObjective(player, "null", DisplaySlot.SIDEBAR);
                     Metadata.provideForPlayer(player).put(SCOREBOARD_OBJECTIVE_METADATA_KEY, scoreboardObjective);
                     updater.accept(player, scoreboardObjective);
-                }).bindWith(terminableConsumer);
+                }, 4)).bindWith(terminableConsumer);
 
         Events.subscribe(PlayerQuitEvent.class)
                 .handler(event -> Metadata.provideForPlayer(event.getPlayer()).remove(SCOREBOARD_OBJECTIVE_METADATA_KEY)).bindWith(terminableConsumer);
@@ -76,5 +87,9 @@ public class ModuleScoreboard implements TerminableModule {
         Schedulers.async().runRepeating(() -> Players.all().forEach(player -> {
             updater.accept(player, Metadata.provideForPlayer(player).getOrNull(SCOREBOARD_OBJECTIVE_METADATA_KEY));
         }), 100L, 100L);
+    }
+
+    public HashSet<UUID> getDisabledScordboard() {
+        return disabledScordboard;
     }
 }
